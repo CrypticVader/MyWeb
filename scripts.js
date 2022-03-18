0ce1274bf8fd025b1b871e42eeb5de348ad83561
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-18 23:30:33 +0530

    Popups (WIP)
    Lotta boilerplate code :D
    lmao

--- scripts.js
+++ scripts.js
@@ -1,9 +1,54 @@
-function OpenModal() {
-    let element = document.getElementById('overlay')
+function OpenMsftModal() {
+    let element = document.getElementById('MsftOverlay')
     element.style.display = 'block'
 }
 
-function CloseModal() {
-    let element = document.getElementById('overlay')
+function CloseMsftModal() {
+    let element = document.getElementById('MsftOverlay')
     element.style.display = 'none'
 }
+//----------------------------------------------------------------------------------------------------------------------
+
+function OpenFreeCcModal() {
+    let element = document.getElementById('FreeCcOverlay')
+    element.style.display = 'block'
+}
+
+function CloseFreeCcModal() {
+    let element = document.getElementById('FreeCcOverlay')
+    element.style.display = 'none'
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function OpenCodCadModal() {
+    let element = document.getElementById('CodCadOverlay')
+    element.style.display = 'block'
+}
+
+function CloseCodCadModal() {
+    let element = document.getElementById('CodCadOverlay')
+    element.style.display = 'none'
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function OpenW3SModal() {
+    let element = document.getElementById('W3SOverlay')
+    element.style.display = 'block'
+}
+
+function CloseW3SModal() {
+    let element = document.getElementById('W3SOverlay')
+    element.style.display = 'none'
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function OpenGitLearnModal() {
+    let element = document.getElementById('GitLearnOverlay')
+    element.style.display = 'block'
+}
+
+function CloseGitLearnModal() {
+    let element = document.getElementById('GitLearnOverlay')
+    element.style.display = 'none'
+}
+//----------------------------------------------------------------------------------------------------------------------
