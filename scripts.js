7e3de0ca8402406a0d0d2dca8ec50673079b11fd
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-20 23:04:59 +0530

    Unified modal open popups

--- scripts.js
+++ scripts.js
@@ -12,10 +12,11 @@ function CloseModal() {
     currentPopup.style.top = '50%'
     document.body.style.animationPlayState = 'running'
 }
+
 //----------------------------------------------------------------------------------------------------------------------
 
-function OpenMsftModal() {
-    currentPopup = document.getElementById('MsftPopup')
+function OpenModal(id) {
+    currentPopup = document.getElementById(id)
     currentPopup.style.top = '20%'
     currentPopup.style.visibility = 'visible'
     currentPopup.style.scale = '1'
@@ -24,52 +25,5 @@ function OpenMsftModal() {
     overlay.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
 }
-//----------------------------------------------------------------------------------------------------------------------
 
-function OpenFreeCCModal() {
-    currentPopup = document.getElementById('FreeCCPopup')
-    currentPopup.style.top = '20%'
-    currentPopup.style.visibility = 'visible'
-    currentPopup.style.scale = '1'
-    currentPopup.style.opacity = '1'
-    overlay.style.opacity = '1'
-    overlay.style.visibility = 'visible'
-    document.body.style.animationPlayState = 'paused'
-}
-//----------------------------------------------------------------------------------------------------------------------
-
-function OpenCodCadModal() {
-    currentPopup = document.getElementById('CodCadPopup')
-    currentPopup.style.top = '20%'
-    currentPopup.style.scale = '1'
-    currentPopup.style.opacity = '1'
-    currentPopup.style.visibility = 'visible'
-    overlay.style.opacity = '1'
-    overlay.style.visibility = 'visible'
-    document.body.style.animationPlayState = 'paused'
-}
-//----------------------------------------------------------------------------------------------------------------------
-
-function OpenW3SModal() {
-    currentPopup = document.getElementById('W3SPopup')
-    currentPopup.style.top = '20%'
-    currentPopup.style.scale = '1'
-    currentPopup.style.opacity = '1'
-    currentPopup.style.visibility = 'visible'
-    overlay.style.opacity = '1'
-    overlay.style.visibility = 'visible'
-    document.body.style.animationPlayState = 'paused'
-}
-//----------------------------------------------------------------------------------------------------------------------
-
-function OpenGitLearnModal() {
-    currentPopup = document.getElementById('GitLearnPopup')
-    currentPopup.style.top = '20%'
-    overlay.style.visibility = 'visible'
-    currentPopup.style.visibility = 'visible'
-    overlay.style.opacity = '1'
-    currentPopup.style.scale = '1'
-    currentPopup.style.opacity = '1'
-    document.body.style.animationPlayState = 'paused'
-}
 //----------------------------------------------------------------------------------------------------------------------
