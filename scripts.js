72bc1e5b03bed5329e3b439cdae161ab2f426393
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-20 16:46:11 +0530

    Decouple Overlay and Popup div

--- scripts.js
+++ scripts.js
@@ -1,54 +1,74 @@
 function OpenMsftModal() {
-    let element = document.getElementById('MsftOverlay')
+    let element = document.getElementById('MsftPopup')
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'block'
     element.style.display = 'block'
 }
 
 function CloseMsftModal() {
-    let element = document.getElementById('MsftOverlay')
+    let element = document.getElementById('MsftPopup')
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'none'
     element.style.display = 'none'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenFreeCcModal() {
-    let element = document.getElementById('FreeCcOverlay')
+    let element = document.getElementById('FreeCcPopup')
     element.style.display = 'block'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'block'
 }
 
 function CloseFreeCcModal() {
-    let element = document.getElementById('FreeCcOverlay')
+    let element = document.getElementById('FreeCcPopup')
     element.style.display = 'none'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'none'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenCodCadModal() {
-    let element = document.getElementById('CodCadOverlay')
+    let element = document.getElementById('CodCadPopup')
     element.style.display = 'block'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'block'
 }
 
 function CloseCodCadModal() {
-    let element = document.getElementById('CodCadOverlay')
+    let element = document.getElementById('CodCadPopup')
     element.style.display = 'none'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'none'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenW3SModal() {
-    let element = document.getElementById('W3SOverlay')
+    let element = document.getElementById('W3SPopup')
     element.style.display = 'block'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'block'
 }
 
 function CloseW3SModal() {
-    let element = document.getElementById('W3SOverlay')
+    let element = document.getElementById('W3SPopup')
     element.style.display = 'none'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'none'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenGitLearnModal() {
-    let element = document.getElementById('GitLearnOverlay')
+    let element = document.getElementById('GitLearnPopup')
     element.style.display = 'block'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'block'
 }
 
 function CloseGitLearnModal() {
-    let element = document.getElementById('GitLearnOverlay')
+    let element = document.getElementById('GitLearnPopup')
     element.style.display = 'none'
+    let overlay = document.getElementById('overlay')
+    overlay.style.display = 'none'
 }
 //----------------------------------------------------------------------------------------------------------------------
