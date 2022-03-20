3bbc6621790d9e569e4ee1b9939823a541357043
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-20 19:12:06 +0530

    Bg animation pause on popup

--- scripts.js
+++ scripts.js
@@ -6,12 +6,14 @@ function OpenMsftModal() {
     let element = document.getElementById('MsftPopup')
     overlay.style.display = 'block'
     element.style.display = 'block'
+    document.body.style.animationPlayState = 'paused'
 }
 
 function CloseMsftModal() {
     let element = document.getElementById('MsftPopup')
     overlay.style.display = 'none'
     element.style.display = 'none'
+    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -19,12 +21,15 @@ function OpenFreeCcModal() {
     let element = document.getElementById('FreeCcPopup')
     element.style.display = 'block'
     overlay.style.display = 'block'
+    document.body.style.animationPlayState = 'paused'
+
 }
 
 function CloseFreeCcModal() {
     let element = document.getElementById('FreeCcPopup')
     element.style.display = 'none'
     overlay.style.display = 'none'
+    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -32,12 +37,14 @@ function OpenCodCadModal() {
     let element = document.getElementById('CodCadPopup')
     element.style.display = 'block'
     overlay.style.display = 'block'
+    document.body.style.animationPlayState = 'paused'
 }
 
 function CloseCodCadModal() {
     let element = document.getElementById('CodCadPopup')
     element.style.display = 'none'
     overlay.style.display = 'none'
+    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -45,12 +52,14 @@ function OpenW3SModal() {
     let element = document.getElementById('W3SPopup')
     element.style.display = 'block'
     overlay.style.display = 'block'
+    document.body.style.animationPlayState = 'paused'
 }
 
 function CloseW3SModal() {
     let element = document.getElementById('W3SPopup')
     element.style.display = 'none'
     overlay.style.display = 'none'
+    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -58,11 +67,13 @@ function OpenGitLearnModal() {
     let element = document.getElementById('GitLearnPopup')
     element.style.display = 'block'
     overlay.style.display = 'block'
+    document.body.style.animationPlayState = 'paused'
 }
 
 function CloseGitLearnModal() {
     let element = document.getElementById('GitLearnPopup')
     element.style.display = 'none'
     overlay.style.display = 'none'
+    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
