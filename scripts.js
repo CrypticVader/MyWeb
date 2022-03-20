bf7e511037f2e49ab80c49af79026e9c8a117cdb
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-20 22:44:57 +0530

    Oopsie
    - Fixed all redirect on popups🤦‍♂️.
    - Unified close functions.
    - Fixed tab font-size.

--- scripts.js
+++ scripts.js
@@ -1,108 +1,69 @@
-let overlay = document.getElementById('overlay')
+var overlay = document.getElementById('overlay')
+var popup
 
+//----------------------------------------------------------------------------------------------------------------------
+
+function CloseModal() {
+    overlay.style.visibility = 'hidden'
+    popup.style.visibility = 'hidden'
+    overlay.style.opacity = '0'
+    popup.style.scale = '0.5'
+    popup.style.opacity = '0'
+    document.body.style.animationPlayState = 'running'
+}
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenMsftModal() {
-    let popup = document.getElementById('MsftPopup')
+    popup = document.getElementById('MsftPopup')
     overlay.style.visibility = 'visible'
     popup.style.visibility = 'visible'
     overlay.style.opacity = '1'
     popup.style.scale = '1'
     popup.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
-}
-
-function CloseMsftModal() {
-    let popup = document.getElementById('MsftPopup')
-    overlay.style.visibility = 'hidden'
-    popup.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    popup.style.scale = '0.5'
-    popup.style.opacity = '0'
-    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
-function OpenFreeCcModal() {
-    let popup = document.getElementById('MsftPopup')
+function OpenFreeCCModal() {
+    popup = document.getElementById('FreeCCPopup')
     overlay.style.visibility = 'visible'
     popup.style.visibility = 'visible'
     overlay.style.opacity = '1'
     popup.style.scale = '1'
     popup.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
-}
-
-function CloseFreeCcModal() {
-    let popup = document.getElementById('MsftPopup')
-    overlay.style.visibility = 'hidden'
-    popup.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    popup.style.scale = '0.5'
-    popup.style.opacity = '0'
-    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenCodCadModal() {
-    let popup = document.getElementById('MsftPopup')
+    popup = document.getElementById('CodCadPopup')
     overlay.style.visibility = 'visible'
     popup.style.visibility = 'visible'
     overlay.style.opacity = '1'
     popup.style.scale = '1'
     popup.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
-}
-
-function CloseCodCadModal() {
-    let popup = document.getElementById('MsftPopup')
-    overlay.style.visibility = 'hidden'
-    popup.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    popup.style.scale = '0.5'
-    popup.style.opacity = '0'
-    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenW3SModal() {
-    let popup = document.getElementById('MsftPopup')
+    popup = document.getElementById('W3SPopup')
     overlay.style.visibility = 'visible'
     popup.style.visibility = 'visible'
     overlay.style.opacity = '1'
     popup.style.scale = '1'
     popup.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
-}
-
-function CloseW3SModal() {
-    let popup = document.getElementById('MsftPopup')
-    overlay.style.visibility = 'hidden'
-    popup.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    popup.style.scale = '0.5'
-    popup.style.opacity = '0'
-    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 function OpenGitLearnModal() {
-    let popup = document.getElementById('MsftPopup')
+    popup = document.getElementById('GitLearnPopup')
     overlay.style.visibility = 'visible'
     popup.style.visibility = 'visible'
     overlay.style.opacity = '1'
     popup.style.scale = '1'
     popup.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
-}
-
-function CloseGitLearnModal() {
-    let popup = document.getElementById('MsftPopup')
-    overlay.style.visibility = 'hidden'
-    popup.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    popup.style.scale = '0.5'
-    popup.style.opacity = '0'
-    document.body.style.animationPlayState = 'running'
 }
 //----------------------------------------------------------------------------------------------------------------------
