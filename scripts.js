d05490370aa0698073cfff145e425ded5f2bb821
Cryptic_Vader <akhilbnair101@gmail.com>
2022-03-24 23:53:56 +0530

    Hot mess

--- scripts.js
+++ scripts.js
@@ -1,21 +1,8 @@
+var currentPopup
+var overflow = document.getElementById('overflow')
 var overlay = document.getElementById('overlay')
 var divider = document.getElementById('divider')
 var headAnimated = document.getElementById('headAnimated')
-var currentPopup
-
-//----------------------------------------------------------------------------------------------------------------------
-
-function CloseModal() {
-    overlay.style.visibility = 'hidden'
-    overlay.style.opacity = '0'
-    currentPopup.style.visibility = 'hidden'
-    currentPopup.style.scale = '0.4'
-    currentPopup.style.opacity = '0'
-    currentPopup.style.top = '50%'
-    document.body.style.animationPlayState = 'running'
-    divider.style.animationPlayState = 'running'
-    headAnimated.style.animationPlayState = 'running'
-}
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -28,8 +15,41 @@ function OpenModal(id) {
     overlay.style.visibility = 'visible'
     overlay.style.opacity = '1'
     document.body.style.animationPlayState = 'paused'
+}
+
+function CloseModal() {
+    overlay.style.visibility = 'hidden'
+    overlay.style.opacity = '0'
+    currentPopup.style.visibility = 'hidden'
+    currentPopup.style.scale = '0.4'
+    currentPopup.style.opacity = '0'
+    currentPopup.style.top = '50%'
+    document.body.style.animationPlayState = 'running'
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function OpenOverflow(id) {
+    overflow = document.getElementById(id)
+    overflow.style.top = '20%'
+    overflow.style.visibility = 'visible'
+    overflow.style.scale = '1'
+    overflow.style.opacity = '1'
+    overflowLayer.style.visibility = 'visible'
+    overflowLayer.style.opacity = '1'
+    document.body.style.animationPlayState = 'paused'
     divider.style.animationPlayState = 'paused'
     headAnimated.style.animationPlayState = 'paused'
 }
 
+function CloseOverflow() {
+    overflowLayer.style.visibility = 'hidden'
+    overflowLayer.style.opacity = '0'
+    overflow.style.visibility = 'hidden'
+    overflow.style.scale = '0.4'
+    overflow.style.opacity = '0'
+    overflow.style.top = '-50%'
+    document.body.style.animationPlayState = 'running'
+    divider.style.animationPlayState = 'running'
+    headAnimated.style.animationPlayState = 'running'
+}
 //----------------------------------------------------------------------------------------------------------------------
