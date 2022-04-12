ddfd4cafc5c7f78b4041524a168d33f55f9cfaba
CrypticVader <akhilbnair101@gmail.com>
2022-04-13 00:35:09 +0530

    support for theme detection & change
    - Should go through the procedure for setting user theme preference once more.

--- scripts.js
+++ scripts.js
@@ -2,175 +2,6 @@ var logData = false;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-var currentPopup;
-var overlay = document.getElementById("overlay");
-var topPercent;
-
-function openModal(id, topHold = "70%") {
-	topPercent = topHold;
-	currentPopup = document.getElementById(id);
-	currentPopup.style.top = "20%";
-	currentPopup.style.opacity = "1";
-	currentPopup.style.transform = "scale(1)";
-	currentPopup.style.visibility = "visible";
-	overlay.style.visibility = "visible";
-	overlay.style.opacity = "1";
-	document.body.style.overflow = "hidden";
-	togglePlayState("pause");
-}
-
-function closeModal() {
-	overlay.style.visibility = "hidden";
-	overlay.style.opacity = "0";
-	currentPopup.style.visibility = "hidden";
-	currentPopup.style.transform = "scale(0.4)";
-	currentPopup.style.opacity = "0";
-	currentPopup.style.top = topPercent;
-	document.body.style.overflow = "auto";
-	togglePlayState("play");
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
-function butttonIconTransition(elementId, iconText, delay = 50) {
-	let element = document.getElementById(elementId);
-	element.style.transform = "scale(0.4)";
-	element.style.opacity = "0.5";
-	setTimeout(function changeIcon() {
-		element.innerHTML = iconText;
-		element.style.transform = "rotate(-180deg)";
-	}, 100);
-	setTimeout(function morphIcon() {
-		element.style.transform = "rotate(-180deg)";
-		element.style.transform = "scale(1)";
-		element.style.opacity = "1";
-	}, 100 + delay);
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
-var popupText = "lorem&nbspipsum";
-
-function spawnAlert(text = popupText, timeout = 1900) {
-	// For 'text' use &nbsp instead of space to avoid line break.
-	var popup = document.createElement("div");
-	popup.innerHTML = text;
-	let margin = text.length * 5.4;
-	popup.className = "alert";
-	popup.style.marginLeft = -margin + "px";
-	document.body.appendChild(popup);
-	transition();
-
-	function transition() {
-		setTimeout(function morph() {
-			popup.style.transform = "scale(1) translate3d(0%, -100%, 0)";
-			popup.style.opacity = "1";
-		}, 0);
-		setTimeout(function remove() {
-			popup.style.transform = "scale(0) translate3d(0%, -100%, 0)";
-			popup.style.opacity = "0";
-		}, timeout);
-		setTimeout(function delDiv() {
-			popup.remove();
-		}, timeout + 300);
-	}
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
-function copyText(text = "undefined") {
-	navigator.clipboard.writeText(text);
-	// Below code is for the icon morph effect
-	toggleIcon();
-
-	function toggleIcon() {
-		let copyIcon = document.getElementById("copyId");
-		copyIcon.style.transform = "scale(0.5)";
-		copyIcon.style.opacity = "0.2";
-		setTimeout(function back2one() {
-			copyIcon.style.transform = "scale(1.25)";
-			copyIcon.style.opacity = "1";
-			copyIcon.innerHTML = "done";
-		}, 300);
-		setTimeout(function fade() {
-			copyIcon.style.opacity = "0.2";
-		}, 1500);
-		setTimeout(function back2copy() {
-			copyIcon.innerHTML = "copy";
-			copyIcon.style.opacity = "1";
-		}, 1800);
-	}
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
-var bgPlayState = "running";
-var bgParticleMotionState;
-var playStateIconText;
-var skipOverride = false;
-
-function togglePlayState(forceState = "toggle") {
-	// Decide what to do based on the current state & forceState
-	if (forceState == "toggle") {
-		if (bgPlayState == "running") {
-			bgPlayState = "paused";
-			bgParticleMotion = "pause";
-			popupText = "Background&nbsppaused";
-			playStateIconText = "play_arrow";
-		} else {
-			bgPlayState = "running";
-			bgParticleMotion = "run";
-			popupText = "Background&nbsprunning";
-			playStateIconText = "pause";
-		}
-		skipOverride = false;
-	} else if (forceState == "play") {
-		skipOverride = bgPlayState == "running" ? true : false;
-		bgPlayState = "running";
-		bgParticleMotion = "run";
-		popupText = "Background&nbsprunning";
-		playStateIconText = "pause";
-	} else if (forceState == "pause") {
-		skipOverride = bgPlayState == "paused" ? true : false;
-		bgPlayState = "paused";
-		bgParticleMotion = "pause";
-		popupText = "Background&nbsppaused";
-		playStateIconText = "play_arrow";
-	}
-	// Error handling
-	else {
-		console.log(
-			`Invalid argument "${forceState}" passed to forceState.
-			Available options: 'toggle', 'pause', 'play'`
-		);
-	}
-	// Log the current state
-	if (logData) {
-		console.log(`bgPlayState: ${bgPlayState}`);
-		console.log(`popupText: ${popupText}`);
-		console.log(`playStateIconText: ${playStateIconText}`);
-		console.log(`skipOverride: ${skipOverride}`);
-		console.log("");
-	}
-	if (skipOverride == false) {
-		// Change play state
-		document.body.style.animationPlayState = bgPlayState;
-		if (bgParticleMotion == "pause") {
-			ballAnims.forEach((elem, i, arr) => {
-				elem.pause();
-			});
-		} else if (bgParticleMotion == "run") {
-			ballAnims.forEach((elem, i, arr) => {
-				elem.play();
-			});
-		}
-		// Icon change & effect
-		butttonIconTransition("playStateIcon", playStateIconText, (delay = 50));
-	}
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
 // Code for adding overflow and navBar to all pages
 
 navBarTemplate = `<img src="/assets/vader-grey.png" height="40px" width="40px" alt="logo"/>
@@ -269,6 +100,26 @@ onclick="togglePlayState(); spawnAlert();"
 >
 	pause
 </span>
+</button>
+
+<button
+id="themeToggleButton"
+style="
+	transition: 0.2s;
+	float: right;
+	vertical-align: middle;
+	padding: 8px;
+	margin-right: 7px;
+"
+onclick="toggleTheme(); spawnAlert('Theme changed');"
+>
+<span
+	id="themeToggleButtonIcon"
+	style="transition: 0.2s"
+	class="material-icons-round md-24"
+>
+	light_mode
+</span>
 </button>`;
 
 overflowMenutemplate = `<a id="overflowMenuHomeButton" class="overflowMenuButton" href="/">
@@ -327,6 +178,255 @@ My projects
 
 document.getElementById("overflowMenuId").innerHTML = overflowMenutemplate;
 document.getElementById("navBar").innerHTML = navBarTemplate;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+var currentPopup;
+var topPercent;
+
+function openModal(id, topHold = "70%") {
+	let overlay = document.getElementById("overlay");
+	topPercent = topHold;
+	currentPopup = document.getElementById(id);
+	currentPopup.style.top = "20%";
+	currentPopup.style.opacity = "1";
+	currentPopup.style.transform = "scale(1)";
+	currentPopup.style.visibility = "visible";
+	overlay.style.visibility = "visible";
+	overlay.style.opacity = "1";
+	document.body.style.overflow = "hidden";
+	togglePlayState("pause");
+}
+
+function closeModal() {
+	let overlay = document.getElementById("overlay");
+	overlay.style.visibility = "hidden";
+	overlay.style.opacity = "0";
+	currentPopup.style.visibility = "hidden";
+	currentPopup.style.transform = "scale(0.4)";
+	currentPopup.style.opacity = "0";
+	currentPopup.style.top = topPercent;
+	document.body.style.overflow = "auto";
+	togglePlayState("play");
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+function butttonIconTransition(elementId, iconText, delay = 50) {
+	let element = document.getElementById(elementId);
+	element.style.transform = "scale(0.4)";
+	element.style.opacity = "0.5";
+	setTimeout(function changeIcon() {
+		element.innerHTML = iconText;
+		element.style.transform = "rotate(-180deg)";
+	}, 100);
+	setTimeout(function morphIcon() {
+		element.style.transform = "rotate(-180deg)";
+		element.style.transform = "scale(1)";
+		element.style.opacity = "1";
+	}, 100 + delay);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+var popupText = "lorem&nbspipsum";
+
+function spawnAlert(text = popupText, timeout = 1900) {
+	// For 'text' use &nbsp instead of space to avoid line break.
+	var popup = document.createElement("div");
+	popup.innerHTML = text;
+	let margin = text.length * 5.4;
+	popup.className = "alert";
+	popup.style.marginLeft = -margin + "px";
+	document.body.appendChild(popup);
+	transition();
+
+	function transition() {
+		setTimeout(function morph() {
+			popup.style.transform = "scale(1) translate3d(0%, -100%, 0)";
+			popup.style.opacity = "1";
+		}, 0);
+		setTimeout(function remove() {
+			popup.style.transform = "scale(0) translate3d(0%, -100%, 0)";
+			popup.style.opacity = "0";
+		}, timeout);
+		setTimeout(function delDiv() {
+			popup.remove();
+		}, timeout + 300);
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+function copyText(text = "undefined") {
+	navigator.clipboard.writeText(text);
+	// Below code is for the icon morph effect
+	toggleIcon();
+
+	function toggleIcon() {
+		let copyIcon = document.getElementById("copyId");
+		copyIcon.style.transform = "scale(0.5)";
+		copyIcon.style.opacity = "0.2";
+		setTimeout(function back2one() {
+			copyIcon.style.transform = "scale(1.25)";
+			copyIcon.style.opacity = "1";
+			copyIcon.innerHTML = "done";
+		}, 300);
+		setTimeout(function fade() {
+			copyIcon.style.opacity = "0.2";
+		}, 1500);
+		setTimeout(function back2copy() {
+			copyIcon.innerHTML = "copy";
+			copyIcon.style.opacity = "1";
+		}, 1800);
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+var bgPlayState = "running";
+var bgParticleMotionState;
+
+function togglePlayState(forceState = "toggle") {
+	// Decide what to do based on the current state & forceState
+	let skipOverride;
+	let playStateIconText;
+	if (forceState == "toggle") {
+		if (bgPlayState == "running") {
+			bgPlayState = "paused";
+			bgParticleMotion = "pause";
+			popupText = "Background&nbsppaused";
+			playStateIconText = "play_arrow";
+		} else {
+			bgPlayState = "running";
+			bgParticleMotion = "run";
+			popupText = "Background&nbsprunning";
+			playStateIconText = "pause";
+		}
+		skipOverride = false;
+	} else if (forceState == "play") {
+		skipOverride = bgPlayState == "running" ? true : false;
+		bgPlayState = "running";
+		bgParticleMotion = "run";
+		popupText = "Background&nbsprunning";
+		playStateIconText = "pause";
+	} else if (forceState == "pause") {
+		skipOverride = bgPlayState == "paused" ? true : false;
+		bgPlayState = "paused";
+		bgParticleMotion = "pause";
+		popupText = "Background&nbsppaused";
+		playStateIconText = "play_arrow";
+	}
+	// Error handling
+	else {
+		console.log(
+			`Invalid argument "${forceState}" passed to forceState.
+			Available options: 'toggle', 'pause', 'play'`
+		);
+	}
+	// Log the current state
+	if (logData) {
+		console.log(`bgPlayState: ${bgPlayState}`);
+		console.log(`popupText: ${popupText}`);
+		console.log(`playStateIconText: ${playStateIconText}`);
+		console.log(`skipOverride: ${skipOverride}`);
+		console.log("");
+	}
+	if (skipOverride == false) {
+		// Change play state
+		document.body.style.animationPlayState = bgPlayState;
+		if (bgParticleMotion == "pause") {
+			ballAnims.forEach((elem, i, arr) => {
+				elem.pause();
+			});
+		} else if (bgParticleMotion == "run") {
+			ballAnims.forEach((elem, i, arr) => {
+				elem.play();
+			});
+		}
+		// Icon change & effect
+		butttonIconTransition("playStateIcon", playStateIconText, (delay = 50));
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Set initial theme based on system preferences, & setup localStorage item
+function initializeTheme() {
+	// if (localStorage.getItem("theme") === null) {}
+	if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches
+	) {
+		localStorage.setItem("theme", "dark");
+		document.getElementById("themeToggleButtonIcon").innerHTML = "light_mode";
+	} else if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: light)").matches
+	) {
+		localStorage.setItem("theme", "light");
+		document.getElementById("themeToggleButtonIcon").innerHTML = "dark_mode";
+	} else {
+		localStorage.setItem("theme", "light");
+		document.getElementById("themeToggleButtonIcon").innerHTML = "light_mode";
+	}
+	document.body.className = localStorage.getItem("theme");
+}
+
+function toggleTheme(forceTheme = "toggle") {
+	// Decide what to do based on the current theme & forceTheme
+	let skipOverride;
+	let popupText;
+	let themeIconText;
+	if (forceTheme == "toggle") {
+		if (localStorage.getItem("theme") == "light") {
+			localStorage.setItem("theme", "dark");
+			popupText = "Dark&nbsptheme&nbspselected";
+			themeIconText = "light_mode";
+		} else if (localStorage.getItem("theme") == "dark") {
+			localStorage.setItem("theme", "light");
+			popupText = "Light&nbsptheme";
+			themeIconText = "dark_mode";
+		}
+		skipOverride = false;
+	} else if (forceTheme == "dark") {
+		skipOverride = localStorage.getItem("theme") == "dark" ? true : false;
+		localStorage.setItem("theme", "dark");
+		popupText = "Dark&nbsptheme";
+		themeIconText = "light_mode";
+	} else if (forceTheme == "light") {
+		skipOverride = localStorage.getItem("theme") == "light" ? true : false;
+		localStorage.setItem("theme", "light");
+		popupText = "Light&nbsptheme&nbspselected";
+		themeIconText = "dark_mode";
+	}
+	// Error handling
+	else {
+		console.log(`${forceTheme} is not a valid argument.`);
+	}
+	// Log the current state
+	if (logData) {
+		console.log(
+			`localStorage.getItem("theme"): ${localStorage.getItem("theme")}`
+		);
+		console.log(`popupText: ${popupText}`);
+		console.log(`themeIconText: ${themeIconText}`);
+		console.log("");
+	}
+	// Change theme
+	if (skipOverride == false) {
+		document.body.className = localStorage.getItem("theme");
+		butttonIconTransition("themeToggleButtonIcon", themeIconText);
+		balls.forEach((elem, i, arr) => {
+			let colorSet =
+				localStorage.getItem("theme") == "light" ? lightColors : darkColors;
+			elem.style.background =
+				colorSet[Math.floor(Math.random() * colorSet.length)];
+		});
+	}
+}
+
+window.addEventListener("load", initializeTheme);
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -411,13 +511,13 @@ function overflow2NavBarId(overflowId) {
 	}
 }
 
-// Currently displays overflow if innerWidth <= 970px
+// Currently displays overflow if innerWidth <= 1024px
 var remainingNavBarWidth =
-	window.innerWidth - 200 - navBarItemsVisible.length * 185;
+	window.innerWidth - 255 - navBarItemsVisible.length * 185;
 
 function overflowHandler() {
 	remainingNavBarWidth =
-		window.innerWidth - 200 - navBarItemsVisible.length * 185;
+		window.innerWidth - 255 - navBarItemsVisible.length * 185;
 	// Initial log
 	if (logData == true) {
 		console.log(
@@ -435,7 +535,7 @@ function overflowHandler() {
 		overflowMenuItemsVisible.push(navBar2OverflowId(navBarItemsVisible[0]));
 		navBarItemsVisible.splice(0, 1);
 		remainingNavBarWidth =
-			window.innerWidth - 200 - navBarItemsVisible.length * 185;
+			window.innerWidth - 255 - navBarItemsVisible.length * 185;
 		// Log data
 		if (logData == true) {
 			console.log("remainingNavBarWidth: " + remainingNavBarWidth);
@@ -469,7 +569,7 @@ function overflowHandler() {
 		);
 		overflowMenuItemsVisible.splice(overflowMenuItemsVisible.length - 1, 1);
 		remainingNavBarWidth =
-			window.innerWidth - 200 - navBarItemsVisible.length * 185;
+			window.innerWidth - 255 - navBarItemsVisible.length * 185;
 		// Log data
 		if (logData == true) {
 			console.log("remainingNavBarWidth: " + remainingNavBarWidth);
@@ -507,6 +607,7 @@ function overflowMenuToggleButtonHandler() {
 
 overflowHandler();
 window.addEventListener("resize", overflowHandler);
+
 //----------------------------------------------------------------------------------------------------------------------
 // Bg particle fx
 
@@ -537,11 +638,13 @@ window.addEventListener(onload, generateParticles());
 
 function generateParticles() {
 	let containerDiv = document.getElementById("particleContainer"); // Particles container
+	let colorSet =
+		localStorage.getItem("theme") == "light" ? lightColors : darkColors;
 	for (let i = 0; i < numBalls; i++) {
 		let ball = document.createElement("div");
 		ball.classList.add("ball");
 		ball.style.background =
-			lightColors[Math.floor(Math.random() * lightColors.length)]; // Random color
+			colorSet[Math.floor(Math.random() * colorSet.length)]; // Random color
 		ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
 		ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
 		ball.style.width = `${Math.random()}em`;
